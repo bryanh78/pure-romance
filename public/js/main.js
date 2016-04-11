56edae599c204aa49650af98a6cb79d05acaa531
bryan <bryanh1990@gmail.com>
2016-04-11 16:28:27 -0600

    'closer'

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,30 @@
 angular.module("pure",['ngRoute'])
 
 angular.module("pure").config(function($routeProvider){
-
+	$routeProvider
+		.when('/', {
+			templateUrl : '/html/landing.html'
+		})
+		.when('/products', {
+			templateUrl : '/html/products.html'
+		})
+		.when('/contact', {
+			templateUrl : '/html/contact.html'
+		})
+		.when('/blog', {
+			templateUrl : '/html/blog.html'
+		})
+		.when('/party', {
+			templateUrl : '/html/party.html'
+		})
+		.when('/videos', {
+			templateUrl : '/html/videos.html'
+		})
+		// .when('/', {
+		// 	templateUrl : '/html/landing.html'
+		// })
+		// .when('/', {
+		// 	templateUrl : '/html/landing.html'
+		// })
 	
 })
